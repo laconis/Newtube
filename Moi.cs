f0cef48d709a1fbe4025a08c74ea5d5cd3510d3d
Nicolas Bainier <laconis.dev@gmail.com>
2025-06-04 01:57:48 +0200

    Ajout de Moi

---
+++ Moi.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Nicolas
+{
+
+
+
+    class Vide
+    {
+
+        private string titre_Video = string.Empty;
+        private int id;
+        private int cat_Video;    
+       
+        public int Id { get { return id; } }  
+        public int Cat_Video { get { return cat_Video; } set { this.Cat_Video = cat_Video; } }
+
+
+
+    }
+
+
+}
