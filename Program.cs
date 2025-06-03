8958bbd118cb08ebbae9fb933cc9728f8d4f66fb
Nicolas Bainier <laconis.dev@gmail.com>
2025-06-04 01:34:32 +0200

    V1

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            _ = Network.po("de");
+
+          
+
+            //Form2 form = new Form2();
+            //form.ShowDialog();
+
+
+      
+
+          
+            /*
+
+                //Vérification de la connexion SQL
+                if (sqlConnection.SqlCheck())
+                {
+                //Maintenance.getInfoFile();
+                NewMaintenance.deleteRebut();
+                Display.Menu();
+                }
+            */
+
+
+
+
+        }
+
+
+
+
+        }
+    }
+
